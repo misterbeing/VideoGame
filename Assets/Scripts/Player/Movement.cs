910ac2c8589a7aa9d8d86d286e7bd623ca0ff7a7
MohdRizwan <rizwan.razzak333@yahoo.com>
2024-04-13 00:23:20 +0530

    new Player Added

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -8,12 +8,16 @@ public class Movement : MonoBehaviour
     public float jumpForce = 10f;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    private Transform thisPlayersTransform;
+    private Animator playerAnimator;
 
     private Rigidbody2D rb;
     public bool isGrounded;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        thisPlayersTransform = this.transform;
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -39,5 +43,21 @@ public class Movement : MonoBehaviour
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        if (rb.velocity.x > 0 )
+        {
+            thisPlayersTransform.rotation =new Quaternion(thisPlayersTransform.rotation.x,0, thisPlayersTransform.rotation.y, thisPlayersTransform.rotation.x);
+            playerAnimator.SetBool("run", true);
+        }
+
+        else if (rb.velocity.x < 0 )
+        {
+            thisPlayersTransform.rotation = new Quaternion(thisPlayersTransform.rotation.x,180,thisPlayersTransform.rotation.y, thisPlayersTransform.rotation.x);
+            playerAnimator.SetBool("run", true);
+        }
+
+        else 
+        {
+            playerAnimator.Play("PlayerAnimations");
+        }
     }
 }
